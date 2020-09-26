93e7c7f19812a14292505db9b8e28fdc56685b2f
Parth Agrawal <parthagrawal.byt.2007@gmail.com>
2020-09-26 10:55:27 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function onSubmit () {
+	name = document.getElementById("name").value;
+	subject = document.getElementById("subject")[document.getElementById("subject").selectedIndex].id;
+	document.getElementById("form").className = "container d-none";
+	document.getElementById("content").className = "container";
+	document.getElementById("names").innerHTML = name;
+	document.getElementById("subjectss").innerHTML = document.getElementById("subject").value;;
+	$.get("data.txt", function (datas) {
+		data = JSON.parse(datas);
+		var i;
+		for (i = 0; i < data.main.length ; i++) {
+			if (subject == data.main[i].id) {
+				document.getElementById("con").innerHTML = data.main[i].content;
+			}
+		}
+	});
+}
